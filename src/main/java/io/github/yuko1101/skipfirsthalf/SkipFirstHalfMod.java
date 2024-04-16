aff61fd1d145d9687c8c1661f62ae679c2e4337b
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-04-16 09:06:37 +0900

    Refactoring

--- src/main/java/io/github/yuko1101/skipfirsthalf/SkipFirstHalfMod.java
+++ src/main/java/io/github/yuko1101/skipfirsthalf/SkipFirstHalfMod.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package io.github.yuko1101.skipfirsthalf;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.impl.util.log.Log;
 import net.fabricmc.loader.impl.util.log.LogCategory;
 
-public class ExampleMod implements ModInitializer {
+public class SkipFirstHalfMod implements ModInitializer {
 
-    public static final String MOD_NAME = "ExampleMod";
+    public static final String MOD_NAME = "SkipFirstHalfMod";
 
     public static final LogCategory LOG_CATEGORY = LogCategory.create(MOD_NAME);
 
